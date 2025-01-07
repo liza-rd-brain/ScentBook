f035c5719b6d4760fda3f38af99286c4329b8b6b
MinakovaLisa <33760912+MinakovaLisa@users.noreply.github.com>
2025-01-07 21:21:37 +0500

    pinia viutify

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <!-- Header -->
+      <Header />
+  
+      <!-- Page Content -->
+      <Nuxt />
+  
+      <!-- Footer -->
+      <Footer />
+    </div>
+  </template>
+  
+  <script>
+  import Header from '@/components/Header.vue';
+  import Footer from '@/components/Footer.vue';
+  
+  export default {
+    components: {
+      Header,
+      Footer,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add global styles here if needed */
+  </style>
